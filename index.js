4988223e77689091b68fba8d5cc772550a194547
Lee Ruben Valero <vleeruben@gmail.com>
2025-09-06 20:43:37 +0800

    some css

--- index.js
+++ index.js
@@ -1,8 +1,8 @@
-const inputFieldEl = document.getElementById("input-field");
-const addButtonEl = document.getElementById("add-button");
+const inputFieldEl = document.getElementById("input-field")
+const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
     
-    console.log(inputValue);
+    console.log(inputValue)
 })
