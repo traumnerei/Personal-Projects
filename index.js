d846cda16008dd725963e72904b251a14777de49
Lee Ruben Valero <vleeruben@gmail.com>
2025-09-06 22:32:14 +0800

    Connected to firebase

--- index.js
+++ index.js
@@ -1,8 +1,22 @@
+
+import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
+import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
+
+
+const appSettings = {
+    databaseURL: "https://realtime-database-cd1d6-default-rtdb.asia-southeast1.firebasedatabase.app/"
+}
+
+const app = initializeApp(appSettings)
+const database = getDatabase(app)
+const shoppinglistInDB = ref(database,"shoppingList")
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
-    
-    console.log(inputValue)
+
+    push(shoppinglistInDB, inputValue)
+
+    console.log(`${inputValue} added to database`)
 })
