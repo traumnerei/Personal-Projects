e5e2318be2e41f40a881457c0d38b5e95cbc78b4
Lee Ruben Valero <vleeruben@gmail.com>
2025-09-10 22:29:44 +0800

    fixed the error
    Fixed the pushing of data to the database then made the input appear as a list. I also reset the input field after clicking the btn

--- Mobile-App_w/index.js
+++ Mobile-App_w/index.js
@@ -1,26 +1,25 @@
-
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-app.js"
-import {getDatabase, ref, push} from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
-
+import { getDatabase, ref, push } from "https://www.gstatic.com/firebasejs/9.15.0/firebase-database.js"
 
 const appSettings = {
     databaseURL: "https://realtime-database-cd1d6-default-rtdb.asia-southeast1.firebasedatabase.app/"
-}
+} //wasn't able to add data to the db cause the write rule is set to false
 
 const app = initializeApp(appSettings)
 const database = getDatabase(app)
-const shoppinglistInDB = ref(database,"shoppingList")
+const shoppingListInDB = ref(database, "shoppingList")
+
 const inputFieldEl = document.getElementById("input-field")
 const addButtonEl = document.getElementById("add-button")
-const itListEl = document.getElementsById("shopping-list")
+const shoppingListEl = document.getElementById("shopping-list")
 
 addButtonEl.addEventListener("click", function() {
     let inputValue = inputFieldEl.value
+    
+    push(shoppingListInDB, inputValue)
 
-    push(shoppinglistInDB, inputValue)
+    inputFieldEl.value = ""
+    
+    shoppingListEl.innerHTML += `<li>${inputValue}</li>`
 
-    console.log(`${inputValue} added to database`)
-    // Challenge: Append a new <li> with text content inputValue to the 'shopping-list' <ul>
-
-    itListEl.innerHTML += <li>${inputValue}</li>
 })
