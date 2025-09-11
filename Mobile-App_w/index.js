8c22a85aeb208b6111e7e21babdd723ca2f04d3e
Lee Ruben Valero <vleeruben@gmail.com>
2025-09-11 23:12:28 +0800

    prac array

--- Mobile-App_w/index.js
+++ Mobile-App_w/index.js
@@ -31,3 +31,23 @@ function resetValueEl(){
 function addtoList(itemValue){
     shoppingListEl.innerHTML += `<li>${itemValue}</li>`
 }
+
+
+// Challenge: Create a let variable called 'scrimbaUsersEmails' and use one of Object methods to set it equal to an array with the values
+
+// Challenge: Create a let variable called 'scrimbaUsersIDs' and use one of Object methods to set it equal to an array with the keys
+
+// Challenge: Create a let variable called 'scrimbaUsersEntries' and use one of Object methods to set it equal to an array with the both the keys and values
+
+let scrimbaUsers = {
+    "00": "sindre@scrimba.com",
+    "01": "per@scrimba.com",
+    "02": "frode@scrimba.com"
+}
+
+
+let scrimbaUsersEmails = Object.values(scrimbaUsers)
+
+let scrimbaUsersIDs = Object.keys(scrimbaUsers)
+
+let scrimbaUsersEntries = Object.entries(scrimbaUsers)
